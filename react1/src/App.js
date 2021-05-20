abc95dd21b636dd1a5b089f2cdbfa723f4457a6c
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-05-20 15:49:01 -0300

    utilização do useEffect

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,10 +1,29 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+
 
 function App() {
+
+  const [contagem, setContagem] = useState(0);
+
+  useEffect(()=>{
+      if (contagem == 0 ) {
+        document.title = 'Começando a brincadeira'
+      }else{
+        document.title = 'A contagem está em :'+contagem
+      }
+  },[contagem])
+
+  function aumentarAction() {
+    setContagem(contagem+1)
+  }
   return (
     <>
-      <h1>testando agora</h1>
+      <h1>Contagem: {contagem}</h1>
+      <button onClick={aumentarAction}>Apertar</button>
     </>
   );
 }
