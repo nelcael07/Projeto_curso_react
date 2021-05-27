4f5a0056ae064e193fd59d695ce160b463d47bf0
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-05-27 12:23:38 -0400

    reducers em varias telas

--- react1/src/pages/Login.js
+++ react1/src/pages/Login.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
+function Login(props) {
+    function handleRogerio() {
+        props.setName('Rogerio')
+    }
 
-function Login() {
     return(
         <div>
             <h1>Página Login</h1>
             <h3>Tem que fazer o login aqui</h3>
+            O nome é {props.name}
+
+            <br></br>
+            <br></br> 
+            <button onClick={handleRogerio}>Trocar nome para Rogerio</button>
         </div>
     )
 }
-export default Login;
+
+const mapStateToProps = (state) =>{
+    return{
+        name: state.usuario.name
+    }
+}
+
+const mapDispatchToProps = dispatch =>{
+    return{
+        setName:(newName) => dispatch({
+            type:'SET_NAME',
+            payload:{name:newName}
+        })
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
